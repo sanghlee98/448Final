0ea783ab00256af2f2402c0760eedc303e2c2848
sanghlee98 <sanghlee98@gmail.com>
2023-05-01 21:19:41 -0500

    correspondence collection and homography computation complete

--- Code/main.py
+++ Code/main.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy
 import skimage.transform as sktr
+import cv2
 
 # Take 4 allignment pairs manually
 def get_points(im1, im2):
@@ -13,12 +14,13 @@ def get_points(im1, im2):
     plt.imshow(im2)
     p5, p6, p7, p8 = plt.ginput(4)
     plt.close()
-    return (p1, p2, p3, p4, p5, p6, p7, p8)
+    src = np.array([p1, p2, p3, p4])
+    dst = np.array([p5, p6, p7, p8])
+    return src.astype(int), dst.astype(int)
 
 # Read images
-def Read(id, path):
-    #remove * 255 jpg files
-    source = plt.imread(path) * 255
+def Read(path):
+    source = plt.imread(path) / 255
     source = np.clip(source, 0, 255)
     return source
 
@@ -27,10 +29,25 @@ inputDir = '../Images/'
 outputDir = '../Samples/'
 
 # 1. Collect correspondce pairs
-
+im1 = Read(inputDir + "a_1.jpeg")
+im2 = Read(inputDir + "a_2.jpeg")
+src, dst = get_points(im1, im2)
 
 # 2. Generate homography H
-
+# to test whether the computed H is correct: sample, mask = cv2.findHomography(src, dst, mask=None)
+A = [[src[0][0], src[0][1], 1, 0, 0, 0, -src[0][0]*dst[0][0], -src[0][1]*dst[0][0]],
+     [src[1][0], src[1][1], 1, 0, 0, 0, -src[1][0]*dst[1][0], -src[1][1]*dst[1][0]],
+     [src[2][0], src[2][1], 1, 0, 0, 0, -src[2][0]*dst[2][0], -src[2][1]*dst[2][0]],
+     [src[3][0], src[3][1], 1, 0, 0, 0, -src[3][0]*dst[3][0], -src[3][1]*dst[3][0]],
+     [0, 0, 0, src[0][0], src[0][1], 1, -src[0][0]*dst[0][1], -src[0][1]*dst[0][1]],
+     [0, 0, 0, src[1][0], src[1][1], 1, -src[1][0]*dst[1][1], -src[1][1]*dst[1][1]],
+     [0, 0, 0, src[2][0], src[2][1], 1, -src[2][0]*dst[2][1], -src[2][1]*dst[2][1]],
+     [0, 0, 0, src[3][0], src[3][1], 1, -src[3][0]*dst[3][1], -src[3][1]*dst[3][1]],
+    ]
+A = np.array(A)
+b = np.array([dst[0][0], dst[1][0], dst[2][0], dst[3][0], dst[0][1], dst[1][1], dst[2][1], dst[3][1]])
+H = np.reshape(np.concatenate((np.linalg.solve(A, b), [1])), (3, 3))
+print(H)
 
 # 3. Determine bounds of the new combined mage
 
